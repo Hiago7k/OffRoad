e7bf085fd082d22225b82248437c04766fdb1187
Hiago 7k <hiago7k@gmail.com>
2026-08-27 19:17:58 -0300

    starting project

---
+++ src/main/java/br/com/offroad/OffRoad/OffRoadApplication.java
@@ -0,0 +1,13 @@
+package br.com.offroad.OffRoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/offroad/OffRoad/OffRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.offroad.OffRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
